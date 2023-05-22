472eec55562c6cecc51ad7ae93208a5839fe1797
Evandro Caxala <evandro.caxala@maibornwolff.de>
2023-05-22 10:25:14 +0200

    Add videoPlayer Controller

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -15,4 +15,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+    }
 }
+
+private func playVideo(){
+    guard let path=Bundle.main.path(forResource: "DemoVid", ofType: "Mp4") else {
+        debugPrint("Mp4 video file not found")
+        return
+    }
+    
+    let player = AVPlayer(url: URL(fileURLWithPath: path))
+    let playerController = AVPlayerViewController()
+    playerController.player = player
+    present(playerController, animated: true) {
+        player.play()
+        }
+}
+
+
