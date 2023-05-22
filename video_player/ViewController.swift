9b66efda3b1299c9240c722b58369f6bebd8b732
Evandro Caxala <evandro.caxala@maibornwolff.de>
2023-05-22 11:38:52 +0200

    Add video playback

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -10,7 +10,7 @@ import AVKit  // provides interfaces for AV playback
 import AVFoundation // provides access to devicce camera or processing Audio
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,21 +18,23 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-    }
-}
-
-private func playVideo(){
-    guard let path=Bundle.main.path(forResource: "DemoVid", ofType: "Mp4") else {
-        debugPrint("Mp4 video file not found")
-        return
+        playVideo()
     }
     
-    let player = AVPlayer(url: URL(fileURLWithPath: path))
-    let playerController = AVPlayerViewController()
-    playerController.player = player
-    present(playerController, animated: true) {
-        player.play()
+    
+    private func playVideo(){
+        guard let path=Bundle.main.path(forResource: "sampleVid", ofType: "mp4") else {
+            debugPrint("Mp4 video file not found")
+            return
         }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player=player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
 }
 
 
